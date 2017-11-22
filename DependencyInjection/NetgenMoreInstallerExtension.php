3e61598cc1e5047413e2a16094064c5f111f12aa
Edi Modrić <edi.modric@gmail.com>
2017-11-22 13:04:28 +0100

    Remove empty configuration

--- DependencyInjection/NetgenMoreInstallerExtension.php
+++ DependencyInjection/NetgenMoreInstallerExtension.php
@@ -9,14 +9,8 @@ use Symfony\Component\DependencyInjection\Loader;
 
 class NetgenMoreInstallerExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
