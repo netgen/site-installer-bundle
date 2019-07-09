fd80670d3233817b74f26f83b44a2f51467dab72
Edi Modrić <edi.modric@gmail.com>
2019-07-09 16:02:41 +0200

    Use explicit nullable types

--- bundle/Installer/BaseInstaller.php
+++ bundle/Installer/BaseInstaller.php
@@ -14,7 +14,7 @@ abstract class BaseInstaller extends DbBasedInstaller implements Installer
      *
      * Schema file is created with: mysqldump ngsite --no-data > schema.sql
      */
-    protected function importSchemaFile(string $schemaFile, string $controlTableName = null): void
+    protected function importSchemaFile(string $schemaFile, ?string $controlTableName = null): void
     {
         if ($controlTableName !== null && $this->db->getSchemaManager()->tablesExist([$controlTableName])) {
             $this->output->writeln('<comment>Schema already exists in the database, skipping schema import for file <info>' . $schemaFile . '</info></comment>');
@@ -30,7 +30,7 @@ abstract class BaseInstaller extends DbBasedInstaller implements Installer
      *
      * Data file is created with: mysqldump ngsite --no-create-info --extended-insert=false > data.sql
      */
-    protected function importDataFile(string $dataFile, string $controlTableName = null): void
+    protected function importDataFile(string $dataFile, ?string $controlTableName = null): void
     {
         if ($controlTableName !== null) {
             $query = $this->db->createQueryBuilder();
