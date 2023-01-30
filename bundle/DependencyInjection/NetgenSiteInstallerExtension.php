19f7279f6b667ca57d0fb6b8af560f34e65233c7
Edi Modrić <edi.modric@gmail.com>
2023-01-30 18:05:14 +0100

    CS fix

--- bundle/DependencyInjection/NetgenSiteInstallerExtension.php
+++ bundle/DependencyInjection/NetgenSiteInstallerExtension.php
@@ -9,6 +9,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
+use function is_dir;
+
 final class NetgenSiteInstallerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
